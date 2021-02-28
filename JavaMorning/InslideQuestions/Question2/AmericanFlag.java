181558078a951b001904bf9f61ceba38e727b2cc
manihrudhay5536 <78911447+manihrudhay5536@users.noreply.github.com>
2021-02-28 17:44:37 +0530

    Create AmericanFlag.java

---
+++ JavaMorning/InslideQuestions/Question2/AmericanFlag.java
@@ -0,0 +1,18 @@
+package inslidequestions;
+
+public class Americaflag {
+
+	
+	public static void main(String[] agrs)
+	{
+		String patStars1="* * * * * * ==================================";
+	    String patStars2=" * * * * *  ==================================";
+		String patLines="==============================================";
+		for(int i=0;i<4;i++)
+			{System.out.println(patStars1);
+			 System.out.println(patStars2);
+			}
+		for(int i=0;i<6;i++)
+			System.out.println(patLines);
+	}
+}
